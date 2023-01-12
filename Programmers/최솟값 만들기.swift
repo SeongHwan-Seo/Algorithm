289834f9f9a22505f0d18d2d81e5590fc4359cc9
SeongHwan-Seo <seosh5704@gmail.com>
2023-01-12 10:34:39 +0900

    20230112

---
+++ Programmers/최솟값 만들기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int
+{
+    var ans = 0
+    
+    var a = A.sorted(by: <)
+    var b = B.sorted(by: >)
+    
+    for i in 0..<a.count {
+        let sum = a[i] * b[i]
+        ans += sum
+    }
+    
+
+    return ans
+}
